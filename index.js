adb5ebb8ab1c0b9615efc4a651476eaa80b21012
Selin Su Varol <selinsuvarol@gmail.com>
2022-06-02 22:43:44 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,20 +1,50 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  // reverse the input string
+  const reversedWord = word.split('').reverse().join('');
+  // compare the reversed string to the input and return true if they are the same and false if they are not
+  return word === reversedWord;
 }
 
 /* 
-  Add your pseudocode here
+  if the reversed string is the same as the input
+    return true
+  else
+    return false
 */
 
 /*
-  Add written explanation of your solution here
+  My palindrome function should be returning like a boolean either true or false. 
+  when the input word is same as the reversed input word it returns true else it returns false
+  so I need to return a boolean value. keep in mind that the input word is a string and the reversed word is a string.
+  so I need to convert the input word to a string and then compare the reversed word to the input word.
+  for example the word  cat is going to be reversed to tac. and it will return false.
+  but the word mom is going to be reversed to mom and it will return true.
 */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("mom"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("hi"));
 
   console.log("");
 
